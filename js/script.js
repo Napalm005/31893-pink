acaa58faa1f9b9c1c1f2c89354fa3abd67772aa5
Napalm005 <Napalm_08@mail.ru>
2015-12-21 12:52:52 +0300

    fix общее №Б2. Грубые ошибки в разметке отсутствуют. [AA]: все скрипты должны быть во внешних файлах

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+var toggler = document.querySelector(".page-header__toggler");
+var menu = document.querySelector(".main-nav");
+var menu_top = document.querySelector(".page-header__top");
+var popup = document.querySelector(".popup");
+var form_send = document.querySelector(".form__submit");
+
+toggler.addEventListener("click", function(event) {
+  event.preventDefault();
+  menu.classList.toggle("main-nav--show");
+  menu_top.classList.toggle("page-header__top--c_dark");
+  toggler.classList.toggle('page-header__toggler--close');
+});
+
+
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: '.swiper-pagination',
+  paginationClickable: true,
+
+  // Navigation arrows
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+})
