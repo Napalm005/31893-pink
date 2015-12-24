6746a35b87b3b849f62ec32b149e86313271aa0b
Napalm005 <Napalm_08@mail.ru>
2015-12-24 12:59:44 +0300

    js: отрефакторил код. Убрал ошибки

--- source/js/number.js
+++ source/js/number.js
@@ -1,18 +1,13 @@
 (function() {
 
   var elements = document.querySelectorAll(".number");
-
-
+  var companion = document.querySelector(".companion");
+  var area = document.querySelector(".companion-wrap");
   var template = document.querySelector("#companion-template").innerHTML;
-  var number = 0;
-  var div = 0;
 
   for (var i = 0; i < elements.length; i++) {
     initNumberField(elements[i]);
   }
-
-
-
 
   function initNumberField(parent) {
     var input = parent.querySelector("input");
@@ -20,36 +15,18 @@
     var plus = parent.querySelector(".number__button--plus");
     var min = parseInt(input.getAttribute('min')) || 0;
     var max = parseInt(input.getAttribute('max'));
-    var companion = document.querySelector(".companion");
-    var del = document.querySelector(".companion__delete");
-    var area = document.querySelector(".form__fieldset--companions");
-
-    del.addEventListener("click", function(event) {
-      debugger;
-      event.preventDefault();
-      companion.classList.add("companion--hidden");
-    });
 
     if (min>max) {
-      min = Math.min(max,min);
-      max = Math.max(max,min);
+      var x = min;
+      min = max;
+      max = x;
     }
 
-  //   del.addEventListener('click', function(event){
-  // debugger;
-
-  //     // area.removeChild(companion);
-  //     for (var i = 0; i < elements.length; i++) {
-  //       var area = document.querySelector(".form__fieldset--companions");
-  //     }
-
-  //   });
-
     minus.addEventListener('click', function(){
-      changeNumber(false);
       if (input == document.getElementById("INPUTDURATION-2")) {
         removeFields();
       }
+      changeNumber(false);
     });
 
     plus.addEventListener('click', function(){
@@ -61,26 +38,24 @@
 
     function changeNumber(operation) {
       var value = parseInt(input.value);
-      var result;
       if (operation) {
-        result = value + 1;
+        value = value + 1;
         if (!isNaN(max)){
-          result = Math.min(result, max);
+          value = Math.min(value, max);
         }
       }  else {
-        result = value - 1;
-        result = Math.max(result, min);
+        value = value - 1;
+        value = Math.max(value, min);
       }
-      input.value = result;
+      input.value = value;
     }
 
     function removeFields() {
-      if (input.value > 0) {
+      var value = parseInt(input.value);
+      if (value > 0) {
         var children = area.children;
         var lastChildNumber = children.length - 1;
         area.removeChild(children[lastChildNumber]);
-      } else {
-        input.value = 1;
       }
     }
 
@@ -97,7 +72,5 @@
 
       area.appendChild(templateElement);
     }
-
-
   }
 })();
